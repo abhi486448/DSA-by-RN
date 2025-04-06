28da864612ef9da1c382dfe523a1c07ec8fb4e9e
abhi486448 <ag096129abhishek@gmail.com>
2025-04-06 08:44:16 -0700

    secomd commit

---
+++ First.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<"Hello Coder Army"<<endl<<"Hello Coder Army";
+    // cout<<3*2;
+
+    //varable decleare 4 bytes
+    // int a = 10, b = 20, c = 30, d, e = 11;
+    // cout<<a+c;
+
+    // char c = 'a';
+    // cout<<c;
+
+    // float f = 2.31;
+    // cout<<f;
+
+    // double d = 3.121;
+    // cout<<d;
+
+    bool b = false;
+    cout<<b;
+};
