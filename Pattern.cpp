9f27725b84b8d67c871856568d07aa74d9112d4b
abhi486448 <ag096129abhishek@gmail.com>
2025-04-24 19:21:40 +0530

    Add L of Pattern

---
+++ Pattern.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // print * 5times in 5 rows
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // print 10 5times in 4 rows
+    // for(int r=1; r<=4; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<"10 ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // print no. of row 5times in 5 rows
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<r<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // print no. of colum in every colum of row
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<c<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // print no. of colum in every colum of row in revers order
+    // for(int r=1; r<=5; r++){
+    //     for(int c=5; c>=1; c--){
+    //         cout<<c<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // print no. of colums square in every colum of row
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<c*c<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // print no. of colums cube in every colum of row
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<c*c*c<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // print alphabets 5times in every row from a to e
+    // char a = 'a';
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<a<<" ";
+    //     }
+    //     cout<<endl;
+    //     a++;
+    // }
+
+    // print alphabets from a to e in every 5 row
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         char a = 'a' + (c-1);
+    //         cout<<a<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //or
+
+    // for(int r=1; r<=5; r++){
+    //     for(char c='a'; c<='e'; c++){
+    //         cout<<c<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // print no. from 1 to 25 in 5*5 matrix
+    // int a=1;
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<a<<" ";
+    //         a++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    //or
+
+    // for(int r=1; r<=5; r++){
+    //     for(int c=1; c<=5; c++){
+    //         cout<<(r-1)*5+c<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    return 0;
+}
