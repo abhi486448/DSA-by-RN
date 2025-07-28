e88eed22705bedc158517d2dddf62059f0a7b6e2
abhi486448 <ag096129abhishek@gmail.com>
2025-07-28 16:51:08 +0530

    Add L-10

---
+++ L-10 Operators/Operators.cpp
@@ -0,0 +1,156 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+
+    // int-int = int
+    // cout << 13/2;
+
+    //float-int = float 
+    //or int-float = float 
+    //or float-float = float
+    // cout << 13.3 / 2;
+    // cout << 13 / 2.1;
+    // cout << 13.3 * 2.3;
+
+    //post increment
+    // int b, a=10;
+    // b = a++;
+    // cout << b <<" ";
+    // cout << a;
+
+    //pre increment
+    // int b, a=10;
+    // b = ++a;
+    // cout << b <<" ";
+    // cout << a;
+
+    //post decrement
+    // int b, a=10;
+    // b = a--;
+    // cout << b <<" ";
+    // cout << a;
+
+    //pre decrement
+    // int b, a=10;
+    // b = --a;
+    // cout << b <<" ";
+    // cout << a;
+
+    //Compersion operator
+    // 0 or 1
+
+    // int a, b;
+    // cout << "Enter a: ";
+    // cin >> a;
+    // cout << "Enter b: ";
+    // cin >> b;
+    
+    // ==
+    // if(a == b)
+    //     cout <<"Yes"; 
+    // else
+    //     cout << "No";
+
+    // !=
+    // if(a != b)
+    //     cout <<"Yes"; 
+    // else
+    //     cout << "No";
+
+    // >
+    // if(a > b)
+    //     cout <<"Yes"; 
+    // else
+    //     cout << "No";
+
+    // <
+    // if(a < b)
+    //     cout <<"Yes"; 
+    // else
+    //     cout << "No";
+
+    // <=
+    // if(a <= b)
+    //     cout <<"Yes"; 
+    // else
+    //     cout << "No";
+
+    //Logical Operator
+    //and &&
+
+    // int a, b, c;
+    // cin>>a>>b>>c;
+
+    // if(a>b && a>c)
+    // cout<<"Yes";
+    // else
+    // cout<<"No";
+
+    //or ||
+
+    // char name;
+    // cin >>name;
+
+    // if(name=='a'||name=='e'||name=='i'||name=='o'||name=='u')
+    //     cout<<"Vowel";
+    // else
+    //     cout<<"Comsonant";
+
+    //not !
+
+    // cout<< !25 <<endl;
+    // cout<< !0 <<endl;
+
+    //Bitwise Operators
+
+    // & operator
+    // int ans = 2&3;
+    // cout<<ans;
+
+    // | operator
+    // int ans = 2|3;
+    // cout<<ans;
+
+    // << operator
+    // int ans = 6 << 1;
+    // cout<< ans;
+
+    // int ans = -6<<2;
+    // cout<< ans;    
+
+    // ^ operator
+    // int ans = 2^3;
+    // cout<< ans;
+
+    // >> operator
+    // int ans = 6 >> 1;
+    // cout<< ans;
+
+    // ~ operator
+    // int ans = ~6;
+    // cout<< ans;
+
+    // Assignment operator
+
+    // int a=10;
+    // a+=15;
+    // cout<<a<<endl;
+    // a-=2;
+    // cout<<a<<endl;
+    // a*=3;
+    // cout<<a<<endl;
+    // a/=2;
+    // cout<<a<<endl;
+    // a%=4;
+    // cout<<a<<endl;
+
+    //ex
+    if(8>5>3<2*4)
+        cout<<"Yes";
+    else
+        cout<<"No";
+
+    return 0;
+}
